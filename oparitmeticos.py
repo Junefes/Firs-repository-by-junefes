f2c748e879f8ab3664672e9e9b778cb92c5e1115
Junefes <fabriziosaavedramoreno855@gmail.com>
2025-05-28 19:42:36 -0500

    Add files via upload

---
+++ oparitmeticos.py
@@ -0,0 +1,33 @@
+#suma y resta (+ y -)
+suma = 12+5
+resta = 12-5
+
+#multiplicacion y division (* y /)
+multiplicacion = 12*5
+division = 12/5
+
+#potencia y raiz ( ** y **0.5)
+potencia = 12**5
+raiz = 12**0.5
+
+#modulo y division entera (% y //)
+modulo = 12 % 5
+division_entera = 12 // 5
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("Potencia:", potencia)
+print("Raíz:", raiz)
+print("Módulo:", modulo)
+print("División entera:", division_entera)
+
+dict = {
+    'nombre': 'June',
+    'edad' : 99,
+    'lenguaje': 'Python',
+}
+
+tipo_dato = type(division)
+print(tipo_dato)
