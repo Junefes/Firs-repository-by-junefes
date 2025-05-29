77661153c959226f999d650dc5b79c3cd6e633dc
Junefes <fabriziosaavedramoreno855@gmail.com>
2025-05-28 20:00:37 -0500

    Add files via upload

---
+++ opcomparacion.py
@@ -0,0 +1,34 @@
+'''igual = 6 == 5
+print(igual,'\n')
+
+distinto = 6 != 5
+print(distinto,'\n')
+
+mayor = 6 > 5
+print(mayor,'\n')
+
+menor = 6 < 5
+print(menor,'\n')
+
+menor_igual = 6 <= 5
+print(menor_igual,'\n')
+
+mayor_igual = 6 >= 5
+print(mayor_igual)'''
+
+a = 1
+b = 2
+c = 5
+
+comparacion = a+b == c
+
+usuario = 'JUNE'
+contrasena = '1234'
+input_us = input('ingrese su usuario: ').upper()
+input_con = input('ingrese su contraseña: ')
+
+if input_us == usuario and input_con == contrasena:
+    print('Bienvenido June')
+else:
+    print('usuario o contrasena invalido')
+    
